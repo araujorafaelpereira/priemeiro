f7aa246e528dc3c3e4fa382b493afd6fc17d6eca
Rafael Araujo <araujorafavg@gmail.com>
2022-05-26 07:18:16 -0300

    projeto finalizado

--- src/main/java/Fundamentos/fundamentos.java
+++ src/main/java/Fundamentos/fundamentos.java
@@ -13,10 +13,10 @@ public class fundamentos {
     public static void main(String [] args) {
 
         String opcao;
-        int area = 0; // receber o resultado dos calculos de areas
+        double area = 0; // receber o resultado dos calculos de areas
 
       entrada = new Scanner(System.in) ;// Ele que ligou
-        System.out.println("Escolha o calcula desejado");
+        System.out.println("Escolha o calculo desejado");
         System.out.println("(1) - Area do QUadrado ");
         System.out.println("(2)  - Area do Retangulo");
         System.out.println("(3)  - Area do Triangulo");
@@ -25,12 +25,28 @@ public class fundamentos {
         switch(opcao){
             case"1":
                 area = calcularAreaDoQuadrado();
+                System.out.println("A area e de " + area + " metros quadrados");
+                break;
+
+            case "2":
+                area = calcularAreaDoRetangulo();
+                System.out.println("A area e de " + area + " metros quadrados");
+                break;
+            case "3":
+                area = calcularAreaTriangulo();
+                System.out.println("A area e de " + area + " metros quadrados");
+                break;
+            case "4":
+                area = calcularAreaDoCirculo();
+                System.out.println("A area da circuferencia e de  " + area + " metros ");
                 break;
             default:
-                System.out.println("Valor invalido"+ opcao);
+                System.out.println("Numero invalido");
         }
-      System.out.println("A area e de " + area + " metros quadrados");
-    }
+
+        }
+
+
 
     // Funcao retorna o tipo do metodo que ela vai retorna no nome
     //O metodo não fala o que vai fazer a funcao fala
@@ -42,6 +58,26 @@ public class fundamentos {
 
 
         }
+        public static int calcularAreaDoRetangulo(){
+        System.out.println("Digite o tamanho do lado :");
+        int lado = entrada.nextInt();
+        return lado * lado ;
+    }
+    public static int calcularAreaTriangulo(){
+        System.out.println("Digite o tamanho do lado :");
+        int lado = entrada.nextInt();
+        System.out.println("Digite o tamanho da Altura :");
+        int altura = entrada.nextInt();
+        return lado * altura ;
+    }
+    public static  double calcularAreaDoCirculo(){
+        System.out.println("Digite o raio :");
+       double raio = entrada.nextInt();
+        double diametro = raio * raio;
+        double pi = 3.14;
+        return diametro *  pi ;
+
+    }
 
 
 }
